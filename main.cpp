48006d44dbd6ec952b5911c9440697819f970d94
woodyxiong <zuosixiaoshenxian@gmail.com>
2017-08-19 16:09:19 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "simple.h"
+using namespace std;
+using namespace tinyxml2;
+int main(){
+	cout <<"asdf";
+	return 0;
+}
---
+++ simple.h
@@ -0,0 +1,16 @@
+#include "tinyxml2.h"
+using namespace tinyxml2;
+class tiny;
+class tiny{
+private:
+	const char* xmlpath;
+	XMLDocument xmldocment;
+	XMLElement xmlelement;
+public:
+	
+	child(char *name);
+	getText();
+	setText();
+	
+}
+ 

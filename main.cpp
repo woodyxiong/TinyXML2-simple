8dfd3b30c08da85171eba70a22371382f84bc34c
Woody Xiong <zuosixiaoshenxian@gmail.com>
2017-10-08 14:59:35 +0800

    删除原始方法

--- main.cpp
+++ main.cpp
@@ -4,38 +4,11 @@ using namespace std;
 using namespace tinyxml2;
 const char* xmlpath="example.xml";
 const char* xmlpathError="errorExample.xml";
-void parseTinyXML();
 void parseSimple();
 int main(){
-//    parseTinyXML();
     parseSimple();
-
 	return 0;
 }
-
-void parseTinyXML(){
-    XMLDocument* doc=new XMLDocument;
-    int status=doc->LoadFile(xmlpathError);
-    if(status!=0)
-        cout<<"open failed"<<endl;
-    XMLElement *root=doc->RootElement();
-    XMLElement *user=root->FirstChildElement("user");
-    while(user){
-        string elename=user->GetText();
-        if(elename.compare("user0")==0){
-            XMLElement *email=user->FirstChildElement("email");
-            while(email){
-                XMLElement *dizhi=doc->NewElement("dizhi");
-                email->InsertEndChild(dizhi);
-                email=email->NextSiblingElement();
-            }
-            break;
-        }
-        user=user->NextSiblingElement();
-    }
-    doc->SaveFile(xmlpathError);
-}
-
 void parseSimple(){
     try {
         Simplexml* simplexml;
